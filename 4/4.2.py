10027a09d8bae7811c7affe17eb9dd92d002ee86
Chernikov Kirill <chernikov.ka@phystech.edu>
2021-09-26 16:42:54 +0300

    Изменил options

--- 4/4.2.py
+++ 4/4.2.py
@@ -15,20 +15,20 @@ screen = pygame.display.set_mode((750, 500))
 rect(screen, (255, 177, 129), (0, 0, 750, 500))
 
 
-def options(x0, y0, w, h, alpha, flag_y, surface, flag):
+def options(x0, y0, w, h, alpha, flag_y, surface, screen_0):
     #Поворачивает surface на alpha, переводит размеры в w*h, отражает относительно игрек,
-    #если стоит флаг, ставит в (x0, y0) screen, если стоит флаг
+    #если стоит флаг, ставит в (x0, y0) screen_0 и его возвращает
+    #(Вставляет в screen_0 surface)
     
     surface = pygame.transform.rotate(surface, alpha)
     surface = pygame.transform.scale(surface, (w, h))
     surface = pygame.transform.flip(surface, flag_y, False)
-    if (flag):
-        screen.blit(surface, (x0, y0))
-    return surface
+    screen_0.blit(surface, (x0, y0))
+    return screen_0
 
-def brench_1(x0, y0, kx, ky, flag_y, flag):
+def brench_1(x0, y0, kx, ky, flag_y, screen_0):
     #Первая веточка
-    #Координаты (x0, y0), коэффициент увеличение, флаг на отражение относительно оси игрек, рисует на screen, если стоит флаг
+    #Координаты (x0, y0), коэффициент увеличение, флаг на отражение относительно оси игрек, рисует на screen_0
     w = 400
     h = 600
     surface = pygame.Surface([w, h], pygame.SRCALPHA)
@@ -37,12 +37,11 @@ def brench_1(x0, y0, kx, ky, flag_y, flag):
     ellipse(surface, GREEN, (300, 60, 6.0 * 5, 34 * 5))
     ellipse(surface, GREEN, (360, 65, 6.0 * 5, 34 * 5))
     ellipse(surface, GREEN, (420, 75, 6.0 * 5, 34 * 5))
-    surface = options(x0 - 100, y0 - 40, int(round(w * kx)), int(round(h * ky)), -12, flag_y, surface, flag)
-    return surface, x0 - 100, y0 - 40
+    surface = options(x0 - 100, y0 - 40, int(round(w * kx)), int(round(h * ky)), -12, flag_y, surface, screen_0)
 
-def brench_2(x0, y0, kx, ky, flag_y, flag):
+def brench_2(x0, y0, kx, ky, flag_y, screen_0):
     #Вторая веточка
-    #Координаты (x0, y0), коэффициент увеличение, флаг на отражение относительно оси игрек, рисует на screen, если стоит флаг
+    #Координаты (x0, y0), коэффициент увеличение, флаг на отражение относительно оси игрек, рисует на screen_0
     w = 400
     h = 600
     surface = pygame.Surface([w, h], pygame.SRCALPHA)
@@ -54,12 +53,11 @@ def brench_2(x0, y0, kx, ky, flag_y, flag):
     ellipse(surface, GREEN, (440, 130, 6.0 * 5, 34 * 5))
     ellipse(surface, GREEN, (510, 145, 6.0 * 5, 34 * 5))
     
-    surface = options(x0 - 100, y0 - 50, int(round(w * kx)), int(round(h * ky)), -12, flag_y, surface, flag)
-    return surface, x0 - 100, y0 - 50
+    surface = options(x0 - 100, y0 - 50, int(round(w * kx)), int(round(h * ky)), -12, flag_y, surface, screen_0)
 
-def trunk(x0, y0, kx, ky, flag):
+def trunk(x0, y0, kx, ky, screen_0):
     #Ствол
-    #Координаты (x0, y0), коэффициент масштабирования, рисует на screen, если стоит флаг
+    #Координаты (x0, y0), коэффициент масштабирования, рисует на screen_0
     w, h = 750, 500
     surface = pygame.Surface([w, h], pygame.SRCALPHA)
     rect(surface, GREEN, (254, 221, 8.6, 64))
@@ -67,33 +65,24 @@ def trunk(x0, y0, kx, ky, flag):
     polygon(surface, GREEN, [(259, 173), (265, 176),(259, 213), (254, 210)])
     polygon(surface, GREEN, [(258, 165), (261, 167),(269, 115), (266, 112)])
 
-    surface = options(x0 - 254, y0 - 115, int(round(w * kx)), int(round(h * ky)), 0, False, surface, flag)
-    return surface, x0 - 254, y0 - 115
+    surface = options(x0 - 254, y0 - 115, int(round(w * kx)), int(round(h * ky)), 0, False, surface, screen_0)
 
 def bambuk(x, y, kx, ky):
     #Рисует бамбук
+    #Координаты (x, y), коэффициент масштабирования
     surface = pygame.Surface([750, 500], pygame.SRCALPHA)
+    brench_1(220, 220, 0.4, 0.4, False, surface)
     
-    surface0, x0, y0 = brench_1(220, 220, 0.4, 0.4, False, False)
-    surface.blit(surface0, (x0, y0))
-    
-    surface0, x0, y0 = trunk(254, 115, 1, 1, False)
-    surface.blit(surface0, (x0, y0))
-    
-    surface0, x0, y0 = brench_2(198, 137, 0.41, 0.41, False, False)
-    surface.blit(surface0, (x0, y0))
-    
-    surface0, x0, y0 = brench_1(345, 175, 0.4, 0.4, True, False)
-    surface.blit(surface0, (x0, y0))
-    
-    surface0, x0, y0 =brench_2(360, 100, 0.41, 0.41, True, False)
-    surface.blit(surface0, (x0, y0))
+    trunk(254, 115, 1, 1, surface)
+    brench_2(198, 137, 0.41, 0.41, False, surface)
+    brench_1(345, 175, 0.4, 0.4, True, surface)
+    brench_2(360, 100, 0.41, 0.41, True, surface)
 
-    options(x, y, int(round(750 * kx)), int(round(500 * ky)), 0, False, surface, True)
+    options(x, y, int(round(750 * kx)), int(round(500 * ky)), 0, False, surface, screen)
 
     
-bambuk(0, 0, 1, 1)
-bambuk(-100, -120, 2, 1.3)
+bambuk(0, 0, 1, 1) #Первый бамбук
+bambuk(-100, -120, 2, 1.3) #Второй бамбук
 
 pygame.display.update()
 clock = pygame.time.Clock()
