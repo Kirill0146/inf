44e7a6dbb41c6e70abec916e5f1b1fc0da3e6d69
Chernikov Kirill <chernikov.ka@phystech.edu>
2021-11-01 23:38:57 +0300

    Танк и бомбы переделаны через спрайты. Теперь обрабатывается встреча бомбы и танка, появился взрыв. Это было очень больно переделывать.

--- 8/танк.py
+++ 8/танк.py
@@ -1,7 +1,7 @@
 import math
 from random import choice
 from random import randint
-
+from random import random
 import pygame
 
 
@@ -25,7 +25,7 @@ WIDTH = 800
 HEIGHT = 600
 
 balls = []
-bombs = []
+bombs = pygame.sprite.Group()
 targets = []
 
 class Ball:
@@ -135,24 +135,33 @@ class Ball:
             if (self.x - obj.x) ** 2 + (self.y - obj.y) ** 2 <= (self.r + obj.r) ** 2:
                 return True
             return False
-
-
-class Gun:
-    def __init__(self, screen):
-        """Конструктор класса Gun"""
         
+def update(image, w, h, angle):
+    """
+    Меняет исходные размеры картинки, а затем поворачивает ее
+    w, h: новые размеры картинки
+    angle: угол поворота картинки
+    """
+
+    image = pygame.transform.scale(image, (w, h))
+    image = pygame.transform.rotate(image, angle)
+
+    return image
+
+class Tank:
+    def __init__(self, screen):
+        """Конструктор класса Tank"""
+        
+        pygame.sprite.Sprite.__init__(self)
         self.screen = screen #Экран
         self.f2_power = 10 #Начальная скорость шариков
         self.f2_on = 0 #Начата ли стрельба. 1, если да.
-        self.an = 0 #Начальный угол
+        self.angle = 0 #Начальный угол
         self.color = GREY #Цвет пушки
-        self.b = 7 #Ширина пушки
-        self.L = 20 #Длина пушки
-        self.L_min = 20 #Начальная длина пушки (минимальная)
-        self.L_max = 90 #Максимальная длина пушки
+        self.k = 1 #Коэффициент пропорциональности длины танка
         self.max_power = 100 #Максимальная скорость шарика
         self.type = 1 #Выбранный тип снарядов
-        self.v = 2 #Скорость танка
+        self.v = 3 #Скорость танка
         self.right = 0 #Елит ли танк направо
         self.left = 0 #Едит ли танк налево 
         self.up = 0 #Едит ли танк вверх
@@ -161,6 +170,15 @@ class Gun:
         self.y = 450 #Координата левого нижнего угла танка
         self.mouse_x = 40 #Координаты мыши
         self.mouse_y = 450 #Координаты мыши
+        self.w = 50 #Ширина танка
+        self.h = 50 #Высота танка
+        self.image = pygame.image.load('images/tank.png').convert_alpha() #Картинка танка
+        self.image_start = self.image #Сохраняем исходную экземпляр
+        self.hp = 3 #Жизни танка
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
         
     def fire2_start(self, event):
         self.f2_on = 1
@@ -175,8 +193,8 @@ class Gun:
         global balls
         
         n_ball = Ball(screen, x = self.x, y = self.y)
-        n_ball.vx = self.f2_power * math.cos(self.an)
-        n_ball.vy =  self.f2_power * math.sin(self.an)
+        n_ball.vx = self.f2_power * math.cos(self.angle)
+        n_ball.vy =  self.f2_power * math.sin(self.angle)
         
         if self.mouse_x < self.x:
             n_ball.vx *= -1
@@ -185,7 +203,7 @@ class Gun:
         balls.append(n_ball)
         self.f2_on = 0
         self.f2_power = 10
-        self.L = self.L_min
+        self.k = 1
 
         n_ball.new_ball(self.type)
 
@@ -198,7 +216,7 @@ class Gun:
             if self.f2_power < 100:
                 self.f2_power += 1
             self.color = YELLOW
-            self.L = self.L_min + (self.L_max - self.L_min) * (self.f2_power - self.f2_on) / (self.max_power - self.f2_on)
+            self.k =  (self.f2_power - self.f2_on) / (self.max_power - self.f2_on) + 1
             
         else:
             self.color = BLACK
@@ -208,23 +226,16 @@ class Gun:
         Рисует пушку
         """
         
-        x0, y0 = self.x, self.y #Координаты нижнего левого угла
-        L, b = self.L, self.b #Длина и ширина пушки
-        an = -self.an #угол в радианах
+        x0, y0 = self.x , self.y #Координаты нижнего левого угла
+        angle = -self.angle / math.pi * 180
+        
+        if x0 > self.mouse_x:
+            angle += 180
 
-        if x0 <= self.mouse_x:
-            pass
-        else:
-            L *= -1
-            b *= -1
-            
-        pygame.draw.polygon(self.screen, self.color, [(x0, y0),
-            (x0 + L * math.cos(an), y0 - L * math.sin(an)),
-            (x0 + L * math.cos(an) - b * math.sin(an), y0 - L * math.sin(an) - b * math.cos(an)),
-            (x0 - b * math.sin(an), y0 - b * math.cos(an)),
-            (x0, y0)])
+        
+        self.image = update(self.image_start, int(50 * self.k), 50, angle)
 
-            
+        screen.blit(self.image, (x0 - self.image.get_size()[0] / 2, y0 - self.image.get_size()[1] / 2))
         
     def targetting(self):
         """Прицеливание. Зависит от положения мыши."""
@@ -234,11 +245,11 @@ class Gun:
             self.mouse_x, self.mouse_y = x0, y0
             
             if x0 > self.x:
-                self.an = math.atan((y0-self.y) / (x0-self.x))
+                self.angle = math.atan((y0-self.y) / (x0-self.x))
             elif x0 == self.x:
-                self.an = math.pi / 2
+                self.angle = math.pi / 2
             elif x0 < self.x:
-                self.an = math.atan((y0-self.y) / (x0-self.x))
+                self.angle = math.atan((y0-self.y) / (x0-self.x))
             
         if self.f2_on:
             self.color = YELLOW
@@ -271,36 +282,69 @@ def draw_text(text, x, y, color_text = 'black', font_text = 36):
     screen.blit(text, (x, y))
 
 
-class Bobm:
-    def __init__(self, x, y, vx = 0, vy = 0, ay = 1, color = BLACK, r = 5):
+class Bomb(pygame.sprite.Sprite):
+    def __init__(self, x, y, vx = 0, vy = 0, ay = 0.25, color = BLACK, r = 20):
         """Конструктор класса Bomb"""
         
-        self.vx = vx
-        self.vy = vy
-        self.color = color
-        self.r = r
-        self.ay = ay
-        self.x = x
-        self.y = y
+        pygame.sprite.Sprite.__init__(self)
+        self.vy = vy #Скорость бомбы по игрек
+        self.r = r #Радиус бомбы
+        self.ay = ay #Ускорение бомбы 
+        self.x = x #Координата икс бомбы
+        self.y = y #Координата игрек бомбы
+        self.image = pygame.image.load('images/bomb_2.png').convert_alpha() #Картинка бомбы
+        self.image_start = self.image #Сохраняем исходную экземпляр
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+        self.flag = True
+        self.add(bombs)
+        self.time0 = -999
+        self.time = self.time0
         
     def move(self):
         """Движение бомбы"""
         
-        self.x += self.vx
         self.y += self.vy
         self.vy += self.ay
 
     def draw(self):
         """Рисует бомбу"""
         
-        pygame.draw.circle(
-            self.screen,
-            self.color,
-            (self.x, self.y),
-            self.r
-        )
-    
+        self.image = update(self.image_start, 2 * self.r, 2 * self.r, 0)
+        screen.blit(self.image, (self.x, self.y))
 
+    def meet(self, obj):
+        """Проверка на то, встретились ли бомба и объект obj"""
+        
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+        
+        obj.mask = pygame.mask.from_surface(obj.image)
+        obj.rect = obj.image.get_rect()
+        obj.rect.x = obj.x
+        obj.rect.y = obj.y
+        
+        if pygame.sprite.collide_mask(self, obj):
+            return True
+        return False
+
+    def bum(self, obj):
+        """Взрыв из-за бомбы"""
+
+        if self.meet(obj) and self.flag:
+            self.image_start = pygame.image.load('images/buum.png').convert_alpha() #Картинка взрыва
+            obj.hp -= 1
+            self.flag = False
+            self.time = 0.3
+
+        if (self.time <= 0 and self.time > self.time0) or self.y > HEIGHT:
+            self.kill()
+        
+        
 class Target:
 
     def new_target1(self):
@@ -313,6 +357,8 @@ class Target:
         self.type = 1 #Тип мишени
         self.vx = randint(-10, 10)
         self.vy = randint(-10, 10)
+        self.time = 2 #Время, через которое произойдет выпуск бомбы
+        self.time0 = self.time
         
     def new_target2(self):
         """ Инициализация новой мишени вида 2. """
@@ -328,7 +374,8 @@ class Target:
         self.ax0 = 1 #Начальное ускорение по оси икс
         self.ay0 = 1 #Начальное ускорение по оси игрек
         self.ax, self.ay = self.ax0, self.ay0
-
+        self.time = 10 #Время, через которое произойдет выпуск бомбы
+        self.time0 = self.time
         
     def new_target(self):
         """Создает новую мишень вида 1 или 2"""
@@ -343,8 +390,7 @@ class Target:
             
     def __init__(self):
         """ Параметры объекта, как целое"""
-        
-        self.score = 0 #Счет
+
         self.new_target()
     
     def hit(self, ball, points = 1):
@@ -456,27 +502,35 @@ def score_draw():
     """Рисут счет"""
 
     draw_text('Score: '+ str(SCORE), 10, 10)
+    draw_text('HP: '+ str(tank.hp), 10, 30)
+    
 
 def draw_all():
     """Рисует все объекты заново"""
     
     screen.fill(WHITE)
-    gun.draw()
+    tank.draw()
     score_draw()
     target.draw()
+
+    for b in bombs:
+        b.draw()
     
     for t in targets:
         t.draw()
     
     for b in balls:
         b.draw()
+
+        
     pygame.display.update()
 
+bombs = pygame.sprite.Group()
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 clock = pygame.time.Clock()
-gun = Gun(screen)
+tank = Tank(screen)
 
 
 for i in range(N_TARGETS):
@@ -495,38 +549,38 @@ while not finished:
         if event.type == pygame.QUIT:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            gun.fire2_start(event)
+            tank.fire2_start(event)
         elif event.type == pygame.MOUSEBUTTONUP:
-            gun.fire2_end(event)
+            tank.fire2_end(event)
         elif event.type == pygame.MOUSEMOTION:
-            gun.targetting()
-            gun.draw()
+            tank.targetting()
+            tank.draw()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_1:
-                gun.type = 1 #Обычные снаряды
+                tank.type = 1 #Обычные снаряды
             elif event.key == pygame.K_2:
-                gun.type = 2 #Большие и тяжелые снаряды
+                tank.type = 2 #Большие и тяжелые снаряды
             elif event.key == pygame.K_3:
-                gun.type = 3 #Маленькие и легкие снаряды
+                tank.type = 3 #Маленькие и легкие снаряды
 
             #Обработка движения танка:
             if event.key == pygame.K_RIGHT:
-                gun.right = 1 
+                tank.right = 1 
             if event.key == pygame.K_LEFT:
-                gun.left = 1
+                tank.left = 1
             if event.key == pygame.K_UP:
-                gun.up = 1
+                tank.up = 1
             if event.key == pygame.K_DOWN:
-                gun.down = 1  
+                tank.down = 1  
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT:
-                gun.right = 0 
+                tank.right = 0 
             if event.key == pygame.K_LEFT:
-                gun.left = 0
+                tank.left = 0
             if event.key == pygame.K_UP:
-                gun.up = 0
+                tank.up = 0
             if event.key == pygame.K_DOWN:
-                gun.down = 0
+                tank.down = 0
             
 
     #Обработка без событий:        
@@ -549,11 +603,21 @@ while not finished:
             i += 1
             
     for j in range(len(targets)):
-        targets[j].move()
+        target = targets[j]
+        target.move()
+        target.time -= 1/FPS
+        if target.time <= 0:
+            b = Bomb(target.x, target.y)
+            b.add(bombs)
+            target.time = target.time0 * 2 * (random() - j / len(targets) ** 2)
 
+    for b in bombs:
+        b.move()
+        b.bum(tank)
+        b.time -= 1/FPS
     
-    gun.power_up() #Обновляет скорость, если идет прицеливание
-    gun.move()
+    tank.power_up() #Обновляет скорость, если идет прицеливание
+    tank.move()
     
     TIME -= 1/FPS
     
